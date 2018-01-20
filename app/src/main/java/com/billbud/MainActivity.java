70e9a5ce738370617d1ee55f8d5be4d881fb33fe
Sahil <smarkand@ucsc.edu>
2018-01-20 11:54:16 -0800

    Added new activity files

--- app/src/main/java/com/billbud/MainActivity.java
+++ app/src/main/java/com/billbud/MainActivity.java
@@ -1,7 +1,9 @@
-package com.billbud;
+package com.billbud.marka.billbud;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v){
+        Intent i = new Intent(this, SecondActivity.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/billbud/SecondActivity.java
@@ -0,0 +1,10 @@
+package com.billbud.marka.billbud;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by marka on 1/20/2018.
+ */
+
+public class SecondActivity extends AppCompatActivity{
+}
