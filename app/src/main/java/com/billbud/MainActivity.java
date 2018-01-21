caed1055aaa5879edcd93daef48f5bb4f2ff085e
Sahil <smarkand@ucsc.edu>
2018-01-20 17:30:07 -0800

    new stuff

---
+++ app/src/main/java/com/billbud/MainActivity.java
@@ -0,0 +1,47 @@
+package com.billbud;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+    Button Add, Done;
+    private ArrayList<String> arrayList;
+    private ArrayAdapter<String> adapter;
+    private EditText txtinput;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String names = "";
+        arrayList = new ArrayList<>(Arrays.asList(names));
+        txtinput = (EditText)findViewById(R.id.txtinput);
+        ListView listView = (ListView)findViewById(R.id.listView);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, arrayList);
+        listView.setAdapter(adapter);
+        Add = (Button)findViewById(R.id.Add);
+        Add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 String newItem = txtinput.getText().toString();
+                 arrayList.add(newItem);
+                 adapter.notifyDataSetChanged();
+            }
+        });
+        Done = (Button)findViewById(R.id.Done);
+    }
+
+        public void onClick(View v) {
+            Intent i = new Intent(this, SecondActivity.class);
+            startActivity(i);
+        }
+}
